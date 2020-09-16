9011ae51a1b4226a6b8ca01e8cbddf8c1010a293
dwannamaker <wannamaker.dylan@gmail.com>
2020-09-16 08:25:16 -0700

    added app.get hola

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send("Hola!");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening for client request on port ${PORT}`);
 });
