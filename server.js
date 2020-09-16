6ac4b6020877ee475c23be3b70aecccd9e57a03c
creattech <crystalsmcneil@gmail.com>
2020-09-16 14:19:25 -0500

    updated serverjs flow

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+/* External Modules */
 const express = require("express");
-const app = express();
-const path = require("path");
 const methodOverride = require("method-override");
+
+/* Internal Modules */
+const db = require("./models");
+const controllers = require("./controllers");
+
+/* Instanced Modules */
+const app = express();
+
+/* Configuration */
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
+/* Middleware */
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+/* Routes */
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
+/* Server Listener */
 app.listen(PORT, () => {
   console.log(`Listening for client request on port ${PORT}`);
 });
