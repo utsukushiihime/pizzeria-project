37e9c9e650f9d464649e197c1c2950ad82b15e28
creattech <crystalsmcneil@gmail.com>
2020-09-18 21:02:11 -0500

    added cc info to user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,24 +3,29 @@ const router = express.Router();
 const db = require("../models");
 const bcrypt = require("bcryptjs");
 
+// base path /
+
 // register form
 router.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
 // register post
-router.get("/register", async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
-    const foundUser = await db.User.findOne({ email: req.body.emai });
+    // search db to see if user already exists (using email)
+    const foundUser = await db.User.findOne({ email: req.body.email });
+    // if a user is found, send back an error
     if (foundUser) {
-      return res.send({ message: "Account already registered" });
+      return res.send({ message: "Account is already registered" });
     }
-
-    //combat rainbow table attacks
+    // if no user is found, hash password
+    // combat rainbow table attacks
     const salt = await bcrypt.genSalt(10);
+    // takes each character and turns it into multiple random characters
     const hash = await bcrypt.hash(req.body.password, salt);
     req.body.password = hash;
-    // create user with req.body and hashed pass
+    // create user with req.body and hashed password
     await db.User.create(req.body);
 
     // redirect to login
@@ -36,30 +41,34 @@ router.get("/login", (req, res) => {
 });
 
 // login post <- authentication
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
+    // see if the user exists (using email)
     const foundUser = await db.User.findOne({ email: req.body.email });
-
+    // if they do not exist, send error
+    if (!foundUser) {
+      return res.send({ message: "Email or Password incorrect" });
+    }
+    // if they do exist, compare db password with entered password using bcrypt (return true/false)
     const match = await bcrypt.compare(req.body.password, foundUser.password);
-
+    // if passwords don't match, send error
     if (!match) {
       return res.send({ message: "Email or Password incorrect" });
-
-      // if passwords match, create session for authentication
-      req.session.currentUser = {
-        username: foundUser.username,
-        id: foundUser._id,
-      };
-
-      // redirect to home
-      res.redirect("/");
     }
+    // if passwords match, create sesssion for authentication
+    req.session.currentUser = {
+      username: foundUser.username,
+      id: foundUser._id,
+    };
+
+    // redirect to home
+    res.redirect("/");
   } catch (error) {
     res.send({ message: "Internal Server Error", err: error });
   }
 });
 
-// logout delete - destory session
+// logout delete <- destroy session
 router.delete("/logout", async (req, res) => {
   await req.session.destroy();
   res.redirect("/");
--- models/User.js
+++ models/User.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    zip: { type: Number, required: true },
+    ccNum: { type: Number, required: true },
+    ccv: { type: Number, required: true },
+    expiry: { type: Number, required: true },
   },
   { timestamps: true }
 );
