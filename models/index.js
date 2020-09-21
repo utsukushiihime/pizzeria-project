40587865415be19f2b96c4a03ae0010286d69485
creattech <crystalsmcneil@gmail.com>
2020-09-21 17:46:04 -0500

    updated useFindAndModify to false to keep from getting deprecation warning

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("Mongodb connected...");
