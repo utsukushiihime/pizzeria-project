997f57ce8881674ba342482ea64b2e34c6b4e859
creattech <crystalsmcneil@gmail.com>
2020-09-26 16:34:52 -0500

    added db mongo connection

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb://localhost:27017/pizzeria";
+const connectionString =
+  "mongodb+srv://admin:n6QHsp438V4f8gK@planet-pizzeria.rauho.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 mongoose
   .connect(connectionString, {
@@ -20,10 +21,10 @@ mongoose.connection.on("disconnect", (event) => {
   console.log("mongodb disconnected", event);
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://admin:n6QHsp438V4f8gK@planet-pizzeria.rauho.mongodb.net/<dbname>?retryWrites=true&w=majority"
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI ||
+//     "mongodb+srv://admin:n6QHsp438V4f8gK@planet-pizzeria.rauho.mongodb.net/<dbname>?retryWrites=true&w=majority"
+// );
 
 module.exports = {
   User: require("./User"),
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const controllers = require("./controllers");
 const app = express();
 
 /* Configuration */
-app.listen(process.env.PORT || 3000);
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 
@@ -63,6 +63,4 @@ app.use("/user", controllers.user);
 app.use("/orders", authRequired, controllers.order);
 
 /* Server Listener */
-app.listen(PORT, () => {
-  console.log(`Listening for client request on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000);
